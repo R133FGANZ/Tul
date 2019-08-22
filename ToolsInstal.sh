51f1436aebd0b3267cc04892b1dcef56f300e6e0
R133FGANZ <50979010+SCTcyber@users.noreply.github.com>
2019-08-22 16:20:13 +0700

    Add files via upload

---
+++ ToolsInstal.sh
@@ -0,0 +1,544 @@
+#Jangan Recode cok / Reedit su
+#Sampe Ketemu Siapa Recode ancur lu semvak
+#Mr.X / Mr.KRTX @Mr.4R13F @Mr.Khacangz @O_Two_ANONY
+#Tidak Ada sistem Yang Tidak Bisa Diretas
+#DevlinCyberTeam #SCTcyber
+#ANONYMOUS_IND
+
+clear
+toilet -f slant --metal "InstalTools"
+sleep 4
+echo '\033[1;31m========================================='
+sleep 0.3
+echo '\033[1;32m+++++++++++++++Mr.4R13F++++++++++++++++++'
+sleep 0.3
+echo '\033[1;32m++++++++++++DevlinCyberTeam++++++++++++++'
+sleep 0.3
+echo '\033[1;31m========================================='
+sleep 3
+
+echo '\033[0;36mTools Si Bodoh Yang Tersakiti'
+sleep 5
+echo '\033[1;37m========================================='
+sleep 0.4
+echo '\033[1;37m1.\033[1;33mLITEDDOS'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m2.\033[1;31mLucitaDdos'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m3.\033[1;34mTorshammer'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m4.\033[1;31mDdos Attack (Trojan)'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m5.\033[1;32mTrojans Mr.Tamvans'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m6.\033[1;34mInstall kali linux'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m7.\033[1;33mInstall LITESPAM'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m8.\033[1;31mInstall SpamSMS'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m9.\033[1;32mTools Daface By Mr.Tamvans'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m10.\033[1;34mHack IG Mr.Tamvans'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m11.\033[1;31mFake Spam Mr.4R13F'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m12.\033[1;32mTembak Kuota'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m13.\033[1;33mHack Pulsa'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m14.\033[1;34mHack Facebook'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m15.\033[1;31mBRUTEFORCE'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m16.\033[1;32mHack Gmail'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m17.\033[1;33mBobol Wifi no root'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m18.\033[1;34mSpam Chat Whatsapp'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m19.\033[1;31mInstall Virus Malicious'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m20.Info Tools!'
+echo '\033[1;37m========================================='
+sleep 0.5
+echo '\033[1;37m0.keluar'
+echo '\033[1;37m========================================='
+sleep 5
+echo '\033[0;36m[$]Pilih nomor      :\033[1;31m'
+read -p "@RootByMr.4R13F@#" nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+clear
+toilet -f slant --gay "LITEDDOS"
+sleep 2
+echo '\033[1;31mCreator : \033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam    : \033[1;37mDevlinCyberTeam'
+sleep 0.2
+echo '\033[1;31mAdmin   : \033[1;37mMr.KRTX'
+sleep 2
+echo '\033[1;33mSedang menginstall'
+echo '\033[1;37mWait...'
+sleep 4
+apt update && upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+chmod +x liteDDOS.py
+clear
+sleep 2
+echo '\033[1;33mTools Telah terinstall'
+echo '\033[1;31mTerimakasih Telah Menggunakan Tools kami'
+echo 'selamat berjuma lagi...'
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+toilet -f slant --gay "LucitaDdos"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+apt update -y
+apt upgrade -y
+apt install git -y
+apt install python2 -y
+apt install toilet figlet -y
+git clone https://github.com/zlucifer/lucita_ddos
+cd lucita_ddos
+chmod +x pukul.py
+sleep 4
+clear
+sleep 2
+echo '\033[1;31mUdah Terisntall coy...(jangan lupa kembali memakai tools kami)'
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+toilet "Torshammer"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg update -y
+pkg upgrade -y
+apt install tor -y
+pkg install python -y
+pkg install python2 -y
+git clone https://github.com/dotfighter/torshammer.git
+cd torshammer
+chmod +x torshammer.py
+sleep4
+clear
+sleep 2
+echo '\033[1;31mTools udah terinstall cok makasih udah pake tools kami'
+fi
+
+if [ $nomor = 4 ] || $nomor = 04 ]
+then
+figlet "Torjans" | lolcat
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg update -y
+pkg upgrade -y
+pkg install python2 -y
+pkg install git -y
+git clone https://github.com/R133F/Death-Tro
+sleep 4
+clear
+sleep 3
+echo '\033[1;31mTerimakasih Selamat mencoba kembali:)'
+fi
+
+if [ $nomor = 5 ] || [ $nomor = 05 ]
+then
+toilet -f slant --gay "Trjan Tamvans"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+apt update && upgrade
+apt install bash
+apt install python python2
+apt install git
+git clone https://github.com/MrTamfanX/MrDdos
+cd MrDdos
+chmod +x *
+sleep 3
+clear
+sleep 2
+echo '\033[1;31mTools udah terinstall selamat mencoba kembali :)'
+fi
+
+if [ $nomor = 6 ] || [ $nomor = 06 ]
+then
+clear
+toilet -f slant --metal "Kali linux"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg update && upgrade -y
+pkg install python2 gnupg git nano
+git clone https://github.com/LionSec/katoolin
+sleep 3
+clear
+echo '\033[1;31mMakasih cok udah pakai tools kami'
+fi
+
+if [ $nomor = 7 ] || [ $nomor = 07 ]
+then
+toilet -f slant --gay "LITESPAM"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg update -y
+pkg upgrade -y
+pkg install git -y
+git clone https://github.com/4L13199/LITESPAM
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mTools Sudah terinstall silahkan mencoba kembali kalo mau'
+sleep 3
+fi
+
+if [ $nomor = 8 ] || [ $nomor = 08 ]
+then
+toilet "SpamSMS"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+apt install git -y
+apt install python2 -y
+apt install php -y
+apt install nano -y
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/Senitopeng/SpamSms.git
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mTools udah terinstall skuy coba lagi:)'
+sleep 3
+fi
+
+if [ $nomor = 9 ] || [ $nomor = 09 ]
+then
+figlet "Daface" | lolcat
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg install git
+pkg install python2
+pkg install python
+git clone https://github.com/MrTamfanX/TebasIndex
+sleep 3
+echo '\033[1;31mTools sudah terinstall mohon di cek'
+sleep 3
+fi
+
+if [ $nomor = 10 ]
+then
+toilet -f slant --metal "HackIG"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+apt update && upgrade -y
+apt install php git
+apt install bash
+git clone https://github.com/MrTamfanX/TamfanFacebook
+cd TamfanFacebook
+chmod +x *
+chmod -R 775 lib
+sh install.sh
+sleep 3
+clear
+echo '\033[1;31mTools udah terinstall mohon di cek ya:)'
+sleep 3
+fi
+
+if [ $nomor = 11 ]
+then
+Toilet -f slant --gay "SpamR133F"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg update && upgrade -y
+pkg install php -y
+pkg install git python2 -y
+pkg install toilet -y
+git clone https://github.com/SCTcyber/SpamR133F.git
+sleep 3
+clear
+sleep3
+echo '\033[1;31mTools telah terinstall selamat mencoba kembali:v'
+fi
+
+if [ $nomor = 12 ]
+then
+figlet "tembak kuota" | lolcat
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+apt update && upgrade -y
+apt install bash -y
+apt install python python2 -y
+apt install git
+apt install nano
+git clone https://github.com/MrTamfanX/TembakKuota
+cd TemabkKuota
+chmod +x *
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mTools telah terinstall mohon dicek su'
+fi
+
+if [ $nomor = 13 ]
+then
+toilet "pulsa"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+apt update && upgrade -y
+apt install php -y
+apt install git -y
+apt install toilet -y
+git clone https://github.com/MrUncle/PulsaGratis
+cd PulsaGratis
+chmod +x *
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mSekian Terimakasih Lelah Gua ngetik cokk'
+fi
+
+if [ $nomor = 14 ]
+then
+toilet "Darkvb"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg update && upgrade -y
+pkg install python2 -y
+pkg install git -y
+pkg install ruby -y
+pkg install toilet -y
+pkg install figlet -y
+pip install --upgrade pip
+pip2 install --upgrade pip2
+pkg install php -y
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/MikoKW/Dark-FBV2.1
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mSalam Dari DevlinCyberTeam'
+fi
+
+if [ $nomor = 15 ]
+then
+toilet -f slant --metal "FACEBOOK"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg update && upgrade -y
+pkg install python2 -y
+pkg install git -y
+pkg install php -y
+pkg install python -y
+pip2 install mechanize
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mTools Sudah terinstall terimaksih sudah pakai tools kami:)'
+fi
+
+if [ $nomor = 16 ]
+then
+toilet "Gmail"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+apt-get update && apt-get upgrade
+apt-get install python python pip-python-setuptools
+apt-get install git
+git clone https://github.com/wifiphisher/wifiphisher.git
+cd wifipisher<
+python setup.py install
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mSelamat Mencoba Kembali'
+fi
+
+if [ $nomor = 17 ]
+then
+echo '\033[1;31mMaaf Gan SC Hack Wifinya Udah Ga work Jadi pending dulu'
+echo '\033[1;31mNanti ada di tools sibodoh yg tersakiti V2'
+fi
+
+if [ $nomor = 18 ]
+then
+toilet "Whatsapp"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+pkg update && upgrade
+pkg install python2 -y
+pkg install git -y
+pkg install php -y
+git clone https://github.com/siputra12/prank.git
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mUdah slesai coey'
+fi
+
+if [ $nomor = 19 ]
+then
+toilet -f slant --metal "Malicious"
+echo '\033[1;31mCreator=\033[1;37mMr.4R13F'
+sleep 0.2
+echo '\033[1;31mTeam   =\033[1;37mDevlinCybeTeam'
+echo '\033[1;31mAdmin  =\033[1;37mMr.KRTX'
+sleep 3
+echo '\033[1;31mSedang menginstall...'
+echo '\033[1;33mWait...'
+sleep 4
+apt update && apt upgrade
+apt install git python2 -y
+git clone https://github.com/Hider5/Malicious
+cd Malicious
+chmod 777 malicious.py
+pip2 install -r requirements.txt
+pip2 install --upgrade pip
+sleep 3
+clear
+sleep 3
+echo '\033[1;31mSudah Terinstall coey cek ajah'
+fi
+
+if [ $nomor = 20 ]
+then
+clear
+toilet -f slant --gay "INFOTOOLS"
+sleep 3
+echo '\033[;33mTools kami dibuat hanya untuk kebaikan'
+sleep 3
+echo '\033[1;31mJika dibuat kejahatan dosa ditanggung sendiri'
+sleep 3
+echo '\033[1;32mSalam Dari \033[1;31mDevlinCyberTeam'
+sleep 3
+echo '\033[1;31mTerimakasih Telah menggunakan Tools kami'
+sleep 4
+echo '\033[1;33m<[==================[+]==================]>'
+fi
+
+if [ $nomor = 0 ]
+echo "selamat tinggal"
+sleep 3
+fi
